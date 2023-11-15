ac81d37f7731d2e327c0cc0b5f7bb579d5257464
Tursunalie <tursunalievolimjon58@gmail.com>
2023-11-15 15:21:37 +0500

    Homework

--- 14_work/14_work(1).py
+++ 14_work/14_work(1).py
@@ -1,4 +1,4 @@
 ### \\\ 1_work
 
-# otam = {'t_yil':'1978','ism':'Turobjon','shahri':'Farg\'ona',}
-# print(f'Otamning ismi {otam['ism']}, {otam["t_yil"]}-yilda, {otam["shahri"]} viloyatida tug\'ilgan')
+otam = {'t_yil':'1978','ism':'Turobjon','shahri':'Farg\'ona',}
+print(f'Otamning ismi {otam['ism']}, {otam["t_yil"]}-yilda, {otam["shahri"]} viloyatida tug\'ilgan')
--- 14_work/14_work(3a).py
+++ 14_work/14_work(3a).py
@@ -1,15 +1,12 @@
 ### \\\ 3_work(a)
 
-# py_lugat = {'integer':'Butun son',
-#         'float':'O\'nlik son',
-#         'list':'Ro\'yhat',
-#         'tuple':'O\'zgarmas ro\'yhat',
-#         'string':'Matn',
-#         'while':'tsikl',
-#         'if':'funktsiya'            
-#         }
-# soz = input('Kalit so\'z kiriting: ')
-# if soz not in py_lugat:
-#     print('Bunday so\'z mavjud emas')
-# else:
-#     print(py_lugat[soz])
+py_lugat = {'integer':'Butun son',
+        'float':'O\'nlik son',
+        'list':'Ro\'yhat',
+        'tuple':'O\'zgarmas ro\'yhat',
+        'string':'Matn',
+        'while':'tsikl',
+        'if':'funktsiya'            
+        }
+soz = input('Kalit so\'z kiriting: ')
+print(py_lugat.get(soz,'Bunday so\'z mavjud emas'))
